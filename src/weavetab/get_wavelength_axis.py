0c53bb62270aa0f1096aab1d961d630545ebf1da
Maria Chillaron <mchillaron@users.noreply.github.com>
2026-01-16 17:20:42 +0100

    Functions get_wavelength_axis and load_arm_datadict created

---
+++ src/weavetab/get_wavelength_axis.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2026 Universidad Complutense de Madrid
+#
+# This file is part of pypistrello.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE
+#
+
+import numpy as np
+
+def get_wavelength_axis(cube):
+    """Generates the wavelength axis from the FITS header information.
+    Parameters
+    ----------
+    cube : dict
+        Dictionary containing the data and headers for the WEAVE cube.
+    Returns
+    -------
+    wavelength : ndarray
+        1D array containing the wavelength values corresponding to each spectral slice.
+    """
+
+    header = cube["data_header"]
+
+    crval3 = header["CRVAL3"]  # reference value
+    crpix3 = header["CRPIX3"]  # reference pixel
+    naxis3 = header["NAXIS3"]  # number of elements along the wavelength axis
+    cdelt3 = header.get("CD3_3", header.get("CDELT3", None))
+    if cdelt3 is None:
+        raise KeyError("CD3_3 nor CDELT3 not found in header.")
+
+    # Standard definition of WCS in FITS
+    indices = np.arange(1, naxis3 + 1)  # FITS counts from 1
+    wavelength = crval3 + (indices - crpix3) * cdelt3
+
+    return wavelength
---
+++ src/weavetab/load_arm_datadict.py
@@ -0,0 +1,83 @@
+#
+# Copyright 2026 Universidad Complutense de Madrid
+#
+# This file is part of pypistrello.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE
+#
+
+from astropy.io import fits
+import numpy as np
+
+
+def load_arm_datadict(filepath):
+    """Load WEAVE cube extensions for a given arm into a data dictionary.
+    The function automatically detects whether the cube corresponds to the RED or BLUE arm.
+    Parameters
+    ----------
+    filepath : Path
+        Path to the WEAVE FITS data cube file.
+    Returns
+    -------
+    cube : dict
+        Dictionary containing the data and headers for the detected arm.
+    arm : str
+        Detected arm ("RED" or "BLUE").
+    """
+
+    cube = {
+        "arm": None,
+        "primary_header": None,
+        "data_header": None,
+        "data": None,
+        "ivar": None,
+        "data_noss": None,
+        "ivar_noss": None,
+        "sensfunc": None,
+    }
+
+    with fits.open(filepath, memmap=False) as hdul:
+
+        # First, detect which arm we are working with
+        arms_found = set()
+        for hdu in hdul:
+            name = hdu.name.upper()
+            if name.endswith("_DATA"):
+                arm = name.replace("_DATA", "")
+                if arm in {"RED", "BLUE"}:
+                    arms_found.add(arm)
+
+        if len(arms_found) == 0:
+            raise ValueError("No RED or BLUE arm found in FITS file.")
+        if len(arms_found) > 1:
+            raise ValueError(f"Multiple arms found: {arms_found}")
+
+        arm = arms_found.pop()
+        cube["arm"] = arm
+        print(f"INFO: Detected arm: {arm}")
+
+        # Secondly, load the extensions into a data dictionary
+        for hdu in hdul:
+            name = hdu.name.upper()
+
+            if name == "PRIMARY":
+                cube["primary_header"] = hdu.header
+
+            elif name == f"{arm}_DATA":
+                cube["data_header"] = hdu.header
+                cube["data"] = hdu.data.astype(np.float32)
+
+            elif name == f"{arm}_IVAR":
+                cube["ivar"] = hdu.data.astype(np.float32)
+
+            elif name == f"{arm}_DATA_NOSS":
+                cube["data_noss"] = hdu.data.astype(np.float32)
+
+            elif name == f"{arm}_IVAR_NOSS":
+                cube["ivar_noss"] = hdu.data.astype(np.float32)
+
+            elif name == f"{arm}_SENSFUNC":
+                cube["sensfunc"] = hdu.data.astype(np.float32)
+
+    return cube, arm
